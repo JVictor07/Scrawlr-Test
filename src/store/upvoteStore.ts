f39c005c94efc86008856143a10ca004e7edd912
João Silva <58793114+JVictor07@users.noreply.github.com>
2025-02-13 13:13:09 -0300

    Persisting on local storage

--- src/store/upvoteStore.ts
+++ src/store/upvoteStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export interface UpvoteRow {
   id: number;
@@ -12,44 +13,51 @@ interface UpvoteStore {
   toggleUpvote: (rowId: number) => void;
 }
 
-export const upvoteStore = create<UpvoteStore>((set) => ({
-  rows: [
+export const upvoteStore = create<UpvoteStore>()(
+  persist(
+    (set) => ({
+      rows: [
+        {
+          id: 1,
+          qtd: 0,
+          isSelected: false,
+        },
+        {
+          id: 2,
+          qtd: 0,
+          isSelected: false,
+        },
+        {
+          id: 3,
+          qtd: 0,
+          isSelected: false,
+        },
+      ],
+      toggleUpvote: (rowId) => {
+        return set((state) => ({
+          rows: state.rows.map((row) => {
+            if (row.id === rowId) {
+              return { ...row, isSelected: !row.isSelected };
+            }
+            return row;
+          }),
+        }));
+      },
+      addUpvote: (rowId) => {
+        console.log('bateu aqui')
+        console.log(rowId)
+        return set((state) => ({
+          rows: state.rows.map((row) => {
+            if (row.id === rowId) {
+              return { ...row, qtd: row.qtd + 1 };
+            }
+            return row;
+          }),
+        }));
+      },
+    }),
     {
-      id: 1,
-      qtd: 0,
-      isSelected: false,
-    },
-    {
-      id: 2,
-      qtd: 0,
-      isSelected: false,
-    },
-    {
-      id: 3,
-      qtd: 0,
-      isSelected: false,
-    },
-  ],
-  toggleUpvote: (rowId) => {
-    return set((state) => ({
-      rows: state.rows.map((row) => {
-        if (row.id === rowId) {
-          return { ...row, isSelected: !row.isSelected };
-        }
-        return row;
-      }),
-    }));
-  },
-  addUpvote: (rowId) => {
-    console.log('bateu aqui')
-    console.log(rowId)
-    return set((state) => ({
-      rows: state.rows.map((row) => {
-        if (row.id === rowId) {
-          return { ...row, qtd: row.qtd + 1 };
-        }
-        return row;
-      }),
-    }));
-  },
-}));
+      name: "upvote-storage",
+    }
+  )
+);
